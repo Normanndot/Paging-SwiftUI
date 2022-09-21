e153f19b989aba8aeddb99b28f34a487ab0db2f6
Norman <normanndot@gmail.com>
2022-09-21 22:58:43 +0530

    Initial Commit

---
+++ XM/XMApp.swift
@@ -0,0 +1,17 @@
+//
+//  XMApp.swift
+//  XM
+//
+//  Created by Norman, ThankaVijay on 21/09/22.
+//
+
+import SwiftUI
+
+@main
+struct XMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
